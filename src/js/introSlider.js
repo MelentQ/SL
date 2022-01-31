1fed0beb17de29cb274b475c64b7ad698c897840
Artem Safin <artemsafin.webdev@gmail.com>
2022-01-31 08:26:09 +0300

    Переделка слайдера

--- src/js/introSlider.js
+++ src/js/introSlider.js
@@ -1,18 +1,38 @@
-import { Swiper, Navigation, Parallax, Pagination } from 'swiper';
+import { Swiper, Navigation, Parallax, Pagination, Controller, EffectFade } from 'swiper';
 
-Swiper.use([Navigation, Parallax, Pagination]);
+Swiper.use([Navigation, Parallax, Pagination, Controller, EffectFade]);
 
 export default function introSlider() {
     const elements = Array.from(document.querySelectorAll('.js-intro-slider'));
 
     elements.forEach(element => {
-        const container = element.querySelector('.swiper-container');
+        const paginationContainer = element.querySelector('.intro__slider-fraction-pagination > .swiper-container');
 
-        new Swiper(container, {
+        const paginationSlider = new Swiper(paginationContainer, {
+            speed: 300,
+            allowTouchMove: false,
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true
+            }
+        });
+
+        const linkLayers = Array.from(element.querySelectorAll('.intro__details-links-layer'));
+
+        const setActiveLinkLayer = index => {
+            linkLayers.forEach(layer => layer.classList.remove('active'));
+
+            linkLayers[index].classList.add('active');
+        };
+
+        const mainContainer = element.querySelector('.intro__slider-inner > .swiper-container');
+
+        const mainSlider = new Swiper(mainContainer, {
             slidesPerView: 'auto',
             watchOverflow: true,
             speed: 1000,
             threshold: 10,
+            longSwipesRatio: 0.3,
             pagination: {
                 el: element.querySelector('.slider-pagination'),
                 type: 'bullets',
@@ -22,7 +42,56 @@ export default function introSlider() {
             navigation: {
                 nextEl: element.querySelector('.intro__slider-arrow--next'),
                 prevEl: element.querySelector('.intro__slider-arrow--prev')
+            },
+            init: false,
+            on: {
+                init: swiper => {
+                    setActiveLinkLayer(swiper.activeIndex);
+                },
+                slideChange: swiper => {
+                    setActiveLinkLayer(swiper.activeIndex);
+                },
+                slideNextTransitionStart: () => {
+                    paginationSlider.slideNext();
+                },
+                slidePrevTransitionStart: () => {
+                    paginationSlider.slidePrev();
+                }
             }
         });
+
+        mainSlider.init();
+
+        const videoContainer = element.querySelector('.intro__video-slider > .swiper-container');
+
+        const playVideo = (slides, activeIndex) => {
+            Array.from(slides).forEach((slide, slideIndex) => {
+                const video = slide.querySelector('video');
+                if (slideIndex === activeIndex) {
+                    video.play();
+                } else {
+                    video.pause();
+                }
+            });
+        };
+
+        const videoSlider = new Swiper(videoContainer, {
+            watchOverflow: true,
+            speed: 600,
+            init: false,
+            allowTouchMove: false,
+            on: {
+                init: swiper => {
+                    playVideo(swiper.slides, swiper.activeIndex);
+                },
+                slideChange: swiper => {
+                    playVideo(swiper.slides, swiper.activeIndex);
+                }
+            }
+        });
+
+        videoSlider.init();
+
+        mainSlider.controller.control = [videoSlider];
     });
 }
