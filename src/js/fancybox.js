5939b30fabce01b809bfce949968339cda9e9828
Albert <kamalov00albert@gmail.com>
2022-07-19 16:35:44 +0300

    Делаю fancybox галлерею

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -4,4 +4,13 @@ export default function fancybox() {
     Fancybox.bind('[data-fancybox]', {
         placeFocusBack: false
     });
+
+    Fancybox.bind('[data-fancybox^="project"]', {
+        placeFocusBack: false,
+        mainClass: 'fancy-project',
+        center: true,
+        Carousel: {
+            slidesPerPage: 1
+        }
+    });
 }
